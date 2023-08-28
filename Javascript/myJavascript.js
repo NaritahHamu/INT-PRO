2de4a9dcd640a77f6b951aaf9d5c07e3237f9244
NariNoodles <104844130+NaritahHamu@users.noreply.github.com>
2023-08-28 17:19:53 +0200

    JavaScript - Service Page
    Set the stage for dynamic javascript. This is the first round. Will change in the future.
    
    Added Basic HTML shell.
    Added Basic JavaScript with test for first tab.

--- Javascript/myJavascript.js
+++ Javascript/myJavascript.js
@@ -1,9 +1,40 @@
-function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: { lat: -33, lng: 151 },
-      disableDefaultUI: true,
-    });
-  }
-  
-  window.initMap = initMap;
+$( document ).ready(function() {
+  console.log( "ready!" );
+});
+
+function ChangeServiceTypeLabels(){
+  document.getElementById("service-type-1-text").innerHTML = "Hi, " + "Mahalo!";
+  document.getElementById("service-type-2-text").innerHTML = "Hi, " + "Mahalo!";
+  document.getElementById("service-type-3-text").innerHTML = "Hi, " + "Mahalo!";
+  document.getElementById("service-type-4-text").innerHTML = "Hi, " + "Mahalo!";
+  document.getElementById("service-type-5-text").innerHTML = "Hi, " + "Mahalo!";
+}
+
+function BlockOne(){
+  document.getElementById("service-title-text").innerHTML = "Building Control Systems";
+  document.getElementById("service-description-text").innerHTML = "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  document.getElementById("service-picture-1").src="/Images/AboutProjects/prj1.png";
+  document.getElementById("service-picture-2").src="/Images/AboutProjects/prj2.png";
+  document.getElementById("service-picture-3").src="/Images/AboutProjects/prj3.png";
+  document.getElementById("service-picture-4").src="/Images/AboutProjects/prj4.png";
+}
+
+function BlockTwo(){
+  document.getElementById("service-title-text").innerHTML = "Facility Managment Service";
+  document.getElementById("service-description-text").innerHTML = "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+}
+
+function BlockThree(){
+  document.getElementById("service-title-text").innerHTML = "FTTH / WAN / LAN Systems";
+  document.getElementById("service-description-text").innerHTML = "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+}
+
+function BlockFour(){
+  document.getElementById("service-title-text").innerHTML = "Lighting Protection Systems";
+  document.getElementById("service-description-text").innerHTML = "4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+}
+
+function BlockFive(){
+  document.getElementById("service-title-text").innerHTML = "Telecommunication Mobile & Fixed";
+  document.getElementById("service-description-text").innerHTML = "5 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+}
